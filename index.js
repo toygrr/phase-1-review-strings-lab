db133bca3d51b1155ed86fba18409dc65138cc69
Tyler <toygrrr@gmail.com>
2022-07-16 15:17:33 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Tyler";
+const welcomeMessage = "Welcome to Flatbook, Tyler!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, TYLER!"
+const shortGreeting = "Welcome, T!"
